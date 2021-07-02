a16d2f324810eef09bca81be83c177fa0350bb54
juanpdoss <juanpdoss@gmail.com>
2021-07-01 21:24:58 -0300

    corrijo referencias

---
+++ FormularioChiquito/FrmPrincipal.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EntidadesModeloSegundoParcial;
+
+namespace FormularioChiquito
+{
+    public partial class FrmPrincipal : Form
+    {
+        private LosHilos hilos;
+
+
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+            this.hilos = new LosHilos();
+            this.hilos.AvisoFin += MostrarMensajeFin;
+        }
+
+        private void btnLanzar_Click(object sender, EventArgs e)
+        {
+            //Al presionar el botón Lanzar se deberá, mediante la sobrecarga del +,
+            //agregar un nuevo hilo al atributo hilos. En caso de error, se mostrará mediante un MessageBox
+
+            try
+            {
+                hilos += 1;
+
+            }
+            catch(CantidadInvalidaException ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+
+
+
+        }
+
+        private void btnBitacora_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(hilos.Bitacora);
+        }
+
+
+        public void MostrarMensajeFin(string mensaje)
+        {
+            MessageBox.Show(mensaje);
+        }
+    }
+}
